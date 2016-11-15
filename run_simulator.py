ed981e880039e42b4dab2fe4629b6dfc6fc64d0d
David Lin <thunderbirdavid@gmail.com>
2016-11-14 16:28:53 -0800

    temp harcoded texture patches

--- run_simulator.py
+++ run_simulator.py
@@ -48,15 +48,17 @@ if __name__ == '__main__':
     # Add the terrain
     TERRAINS.append(Terrain(-2048, -2048, 4096, 1792, 'dirt', screen, SCREEN_SIZE))
     TERRAINS.append(Terrain(-2048, -256, 4096, 128, 'patchy', screen, SCREEN_SIZE))
-    TERRAINS.append(Terrain(-2048, -128, 4096, 256, 'grass', screen, SCREEN_SIZE))
-    for i in random.sample(xrange(0, 32), 12):
-        TERRAINS.append(Terrain(-2048 + i*128, -128 + (i%2) * 128, 128, 128, 'icegrass', screen, SCREEN_SIZE))
+    TERRAINS.append(Terrain(-2048, -128, 4096, 128, 'grass', screen, SCREEN_SIZE))
+    # TEMP: ice block
+    TERRAINS.append(Terrain(-2048, 0, 4096, 128, 'ice', screen, SCREEN_SIZE))
+    #for i in random.sample(xrange(0, 32), 12):
+        #TERRAINS.append(Terrain(-2048 + i*128, -128 + (i%2) * 128, 128, 128, 'icegrass', screen, SCREEN_SIZE))
     TERRAINS.append(Terrain(-2048, 128, 4096, 256, 'road', screen, SCREEN_SIZE))
-    for i in random.sample(xrange(0, 32), 8):
-        TERRAINS.append(Terrain(-2048 + i*128, 128 + (i%2) * 128, 128, 128, 'ice', screen, SCREEN_SIZE))
+    #for i in random.sample(xrange(0, 32), 8):
+        #TERRAINS.append(Terrain(-2048 + i*128, 128 + (i%2) * 128, 128, 128, 'ice', screen, SCREEN_SIZE))
     TERRAINS.append(Terrain(-2048, 384, 4096, 256, 'grass', screen, SCREEN_SIZE))
-    for i in random.sample(xrange(0, 32), 12):
-        TERRAINS.append(Terrain(-2048 + i*128, 384 + (i%2) * 128, 128, 128, 'icegrass', screen, SCREEN_SIZE))
+    #for i in random.sample(xrange(0, 32), 12):
+        #TERRAINS.append(Terrain(-2048 + i*128, 384 + (i%2) * 128, 128, 128, 'icegrass', screen, SCREEN_SIZE))
     TERRAINS.append(Terrain(-2048, 640, 4096, 128, 'patchy', screen, SCREEN_SIZE))
     TERRAINS.append(Terrain(-2048, 768, 4096, 1792, 'dirt', screen, SCREEN_SIZE))
 
