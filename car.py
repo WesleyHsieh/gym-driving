Fixed rotation model, fixed graphics model for terrain, added test cases

car.py
@@ -39,23 +39,21 @@ class Car(Rectangle):
         # Get properties of terrain that the car is currently on
         decel = np.sum([t.decel for t in terrain_collisions])
         slip = np.sum([t.slip for t in terrain_collisions])
+        print "collision textures", [t.texture for t in terrain_collisions]
         print "decel", decel
         print "slip", slip
+        print 'vel', self.vel
+        print 'angle', self.angle
+        print 'width, length', self.width, self.length
+        print 'x,y', self.x, self.y
+        print 'corners', self.get_corners()
 
-        if slip == 0:
-            self.vel_angle = self.angle
         self.angle += action_dict['steer']
         self.angle %= 360.0
+        if slip == 0:
+            self.vel_angle = self.angle
         self.acc = action_dict['acc'] - decel
         self.acc = max(min(self.acc, self.max_vel - self.vel), -self.vel)
-
-    def get_pos(self):
-        """
-        Returns x, y coordinates.
-        :return: tuple
-            x, y coordinates.
-        """
-        return self.x, self.y
 
     def get_state(self):
         state_dict = {}

rectangle.py
@@ -8,18 +8,26 @@ class Rectangle(object):
         self.width = width
         self.length = length
 
+    def get_pos(self):
+        """
+        Returns x, y coordinates.
+        :return: tuple
+            x, y coordinates.
+        """
+        return self.x, self.y
+        
     def get_corners(self):
         """"
         Returns corners of rectangle. 
         :return: None
         """
-        angle = self.angle
+        angle = np.radians(self.angle)
         corner_offsets = np.array([self.width / 2.0, self.length / 2.0])
         centers = np.array([self.x, self.y])
         signs = np.array([[1,1], [1,-1], [-1,1], [-1,-1]])
-        corners = centers + signs * corner_offsets
+        corner_offsets = signs * corner_offsets
         rotation_mat = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-        rotated_corners = np.dot(corners, rotation_mat.T)
+        rotated_corners = np.dot(corner_offsets, rotation_mat.T) + centers
         return rotated_corners
 
     def collide_rect(self, other_rect):