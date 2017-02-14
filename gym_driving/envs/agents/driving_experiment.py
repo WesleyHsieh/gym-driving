740276b598afefdb1d22ca325706606a4e2ddaab
Wesley Hsieh <wesley.y.hsieh@gmail.com>
2017-02-13 17:04:06 -0800

    Added parallelism for trials

--- gym_driving/envs/agents/driving_experiment.py
+++ gym_driving/envs/agents/driving_experiment.py
@@ -18,6 +18,8 @@ import time
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import multiprocessing
+from functools import partial
 
 from deep_lfd.learning_driving.linear_learner import *
 # from deep_lfd.learning_driving.deep_learner import *
@@ -43,51 +45,25 @@ class Experiment():
 
         self.file_path = file_path 
 
-
     def compute_averages(self, stats):
         train_average, test_average, reward_average, surr_loss_average = \
             [np.mean(np.array(stat)) for stat in zip(*stats)]
         return train_average, test_average, reward_average, surr_loss_average
 
-    def save_data(self, stats, agent_name):
-        # trn_avg,tst_avg,reward_avg,surr_loss_avg = self.compute_averages(stats)
-        # print("Average train acc, test acc, reward, surrogate loss")
-        # print(trn_avg,tst_avg,reward_avg,surr_loss_avg)
+    def save_data(self, stats, experiment_name):
         data_filepath = os.path.join(self.file_path, agent_name) + '.pkl'
         pickle.dump(stats, open(data_filepath,'wb'))
-        self.plot_reward_curve(stats, agent_name)
+        self.plot_reward_curve(stats, experiment_name)
 
-    def run_experiment(self, agent, agent_name):
+    def run_experiment(self, learner_name, agent_name):
+        experiment_name = '{}_{}'.format(learner_name, agent_name)
+        partial_func = partial(run_experiment_trial, learner_name=learner_name, agent_name=agent_name, \
+            iterations=self.ITERATIONS, samples_per_rollout=self.SAMPLES_PER_ROLLOUT, samples_per_eval=self.SAMPLES_PER_EVAL)
+        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count() - 1)
+        overall_stats = pool.map(partial_func, range(self.TRIALS))
+        self.save_data(overall_stats, experiment_name)
 
-        overall_stats = []
-
-        for i in range(self.TRIALS):
-            trial_stats = []
-            for j in range(self.ITERATIONS):
-                print("Agent {}: Trial {}, Iteration {}".format(agent_name, i, j))
-                state_list, action_list = [], []
-                # Collect Samples 
-                # print("Rolling out algorithm")
-                for k in range(self.SAMPLES_PER_ROLLOUT):   
-                    states, actions = agent.rollout_algorithm()
-                    state_list.append(states)
-                    action_list.append(actions)
-                # print("Updating Model")
-                agent.update_model(state_list, action_list)
-                # print("Evaluating Policy")
-                for k in range(self.SAMPLES_PER_EVAL):
-                    agent.eval_policy()
-                trial_stats.append(agent.get_statistics())
-            overall_stats.append(trial_stats)
-            print("Stats for Trial {}: ".format(i))
-            print("Average train acc, test acc, reward, surrogate loss")
-            print(overall_stats[i])
-            agent.reset()
-        self.save_data(overall_stats, agent_name)
-
-    def plot_reward_curve(self, stats, agent_name):
-        # stats: (trials, iterations, stats)
-        # means, stds: (iterations, stats)
+    def plot_reward_curve(self, stats, experiment_name):
         stats_names = ['train_loss', 'test_loss', 'reward', 'surrogate_loss']
         means, stds = np.mean(stats, axis=0), np.std(stats, axis=0)
         n_iters, n_stats = means.shape
@@ -96,35 +72,54 @@ class Experiment():
             stats_name = stats_names[i]
             stat_means, stat_stds = means[:, i], stds[:, i]
             plt.errorbar(range(len(stat_means)), stat_means, yerr=stat_stds)
-            plt.title("Driving, Learner: {}, Stat: {}".format(agent_name, stats_name))
+            plt.title("Driving, Learner: {}, Stat: {}".format(experiment_name, stats_name))
             plt.xlabel("Number of Iterations")
             plt.ylabel(stats_name)
-            plt.savefig('stats/stats_{}_{}.png'.format(agent_name, stats_name))
+            plt.savefig('stats/stats_{}_{}.png'.format(experiment_name, stats_name))
         
+def run_experiment_trial(trial_number, learner_name, agent_name, iterations, samples_per_rollout, samples_per_eval):
+    np.random.seed(trial_number)
+    # Set up learner, agent
+    if learner_name == 'linear_learner':
+        learner = LinearLearner()
+    elif learner_name == 'deep_learner':
+        learner = DeepLearner()
+    env = DrivingEnv(graphics_mode=True)
+    supervisor = DrivingAgent()
+    if agent_name == 'supervised':
+        agent = SupervisedAgent(learner, env, supervisor)
+    elif agent_name == 'dagger':
+        agent = DaggerAgent(learner, env, supervisor)
+
+    # Run trial
+    trial_stats = []
+    for j in range(iterations):
+        print("Agent {}: Trial {}, Iteration {}".format(agent_name, trial_number, j))
+        state_list, action_list = [], []
+        # Collect samples 
+        for k in range(samples_per_rollout):   
+            states, actions = agent.rollout_algorithm()
+            state_list.append(states)
+            action_list.append(actions)
+        # Update model
+        agent.update_model(state_list, action_list)
+        # Evaluate policy
+        for k in range(samples_per_eval):
+            agent.eval_policy()
+        trial_stats.append(agent.get_statistics())
+    print("Stats for Trial {}: ".format(trial_number))
+    print("Train acc, Test acc, Reward, Surrogate Loss")
+    print(trial_stats)
+    return trial_stats
+
 if __name__ == '__main__':
     FILEPATH = ''
-    # learners = [('linear_learner', LinearLearner()), ('deep_learner', DeepLearner())]
+    # learners = ['linear_learner', 'deep_learner']
     learners = ['linear_learner']
     agents = ['supervised', 'dagger']
     for learner_name in learners:
-        if learner_name == 'linear_learner':
-            learner = LinearLearner()
-        elif learner_name == 'deep_learner':
-            learner = DeepLearner()
         for agent_name in agents:
-            # learner = LinearLearner()
-            # learner = DeepLearner()
-            start = time.time()
             print('Running experiment with {}'.format(learner_name))
-            env = DrivingEnv(graphics_mode=True)
-            supervisor = DrivingAgent()
-            if agent_name == 'supervised':
-                agent = SupervisedAgent(learner, env, supervisor)
-            elif agent_name == 'dagger':
-                agent = DaggerAgent(learner, env, supervisor)
             exp_class = Experiment(FILEPATH)
-            experiment_name = '{}_{}'.format(learner_name, agent_name)
-            exp_class.run_experiment(agent, experiment_name)
-            end = time.time()
-            print("Time Elapsed: ", end - start)
+            exp_class.run_experiment(learner_name, agent_name)
 
