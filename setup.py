71213a2a7648b22c441e33d26101294662f9e3c8
Wesley Hsieh <wesley.y.hsieh@gmail.com>
2017-04-19 03:23:41 -0700

    Updated setup dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='gym_driving',
       version='0.0.1',
       install_requires=['gym>=0.5.4',
       					'pygame>=1.9.2b8',
+					'opencv-python>=3.2.0.7',
       					'future>=0.16.0']  # And any other dependencies foo needs
 )  
